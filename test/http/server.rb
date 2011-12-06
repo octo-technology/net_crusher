00d7effc1ed2e3338e2fde87427817ddf0b12f22
Bertrand Paquet <bpaquet@octo.com>
2011-12-06 14:26:44 +0100

    Improve http test

--- test/http/server.rb
+++ test/http/server.rb
@@ -20,6 +20,23 @@ get '/last_modified' do
   headers "Last-Modified" => Time.now.to_s
   headers "Etag" => "6789"
   headers "If-Modified-Since" => env["HTTP_IF_MODIFIED_SINCE"] if env["HTTP_IF_MODIFIED_SINCE"]
-  headers "If-None-Macth" => env["HTTP_IF_NONE_MATCH"] if env["HTTP_IF_NONE_MATCH"]
+  headers "If-None-Match" => env["HTTP_IF_NONE_MATCH"] if env["HTTP_IF_NONE_MATCH"]
   body "42"
+end
+
+get '/redirect' do
+  redirect '/end_redirect'
+end
+
+get '/end_redirect' do
+  "out_of_redirect"
+end
+
+get '/protected' do
+  p env
+  if env["HTTP_AUTHORIZATION"]
+    body env["HTTP_AUTHORIZATION"]
+  else
+    status 401
+  end
 end
--- test/http/test_simple.rb
+++ test/http/test_simple.rb
@@ -3,11 +3,16 @@ base_url = "http://localhost:#{port}"
 
 log 0, "Base url : #{base_url}"
 
+log 0, "Simple"
+
 http_get "#{base_url}/hello"
 assert_last_http_response_code_body 200, "42"
+assert_equal 2, last_http_response_size
 
 http_get "#{base_url}/hello2"
 assert_last_http_response_code 404
+
+log 0, "HTTP headers"
 
 http_get "#{base_url}/headers/salut"
 assert_last_http_response_code_body 418, "42"
@@ -20,6 +25,8 @@ assert_last_http_response_code_body 415, "localhost:#{port}"
 # http_get "#{base_url}/read_header/titi", {"Titi" => "myValue"}
 # assert_last_http_response_code_body 415, "myValue"
 
+log 0, "Get with last modified"
+
 http_get_with_last_modified "#{base_url}/last_modified"
 assert_last_http_response_code_body 200, "42"
 last_modified = last_http_response_header("last-modified")
@@ -28,4 +35,23 @@ etag = last_http_response_header("etag")
 http_get_with_last_modified "#{base_url}/last_modified"
 assert_last_http_response_code_body 200, "42"
 assert_equal last_modified, last_http_response_header("if-modified-since")
-# assert_equal etag, last_http_response_header("if-none-match")
+assert_equal etag, last_http_response_header("if-none-match")
+
+log 0, "Redirect"
+
+http_get "#{base_url}/redirect"
+assert_last_http_response_code 302
+assert_http_redirect "#{base_url}/end_redirect"
+follow_redirect
+assert_equal "#{base_url}/end_redirect", last_http_url
+assert_last_http_response_code_body 200, "out_of_redirect"
+
+log 0, "Basic auth"
+
+http_get "#{base_url}/protected"
+assert_last_http_response_code 401
+login = "titi"
+password = "toto"
+set_basic_auth "#{base_url}/protected", "login", "password"
+http_get "#{base_url}/protected"
+assert_last_http_response_code_body 200, "Basic dGl0aTp0b3Rv"

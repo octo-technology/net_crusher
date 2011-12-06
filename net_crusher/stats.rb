4d0bf899abadc76d45d68e5174117c2fa25b4cd6
Bertrand Paquet <bpaquet@octo.com>
2011-12-06 11:19:01 +0100

    Simple stats parsing

---
+++ net_crusher/stats.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+puts "Parse log files #{ARGV}"
+
+stats = {}
+
+ARGV.each do |file|
+  File.open(file).each_line{ |s|
+    if s =~ /(\d+) (\d+) \S+ (\S+)/
+      start, delay, id = $1, $2, $3
+      delay = delay.to_f / 1000
+      stats[id] = {:count => 0, :min => 1073741823, :max => 0, :sum => 0} unless stats[id]
+      stats[id][:count] = stats[id][:count] + 1
+      stats[id][:sum] = stats[id][:sum] + delay
+      stats[id][:min] = delay if delay < stats[id][:min]
+      stats[id][:max] = delay if delay > stats[id][:max]
+    else
+      raise "Wrong line #{s}"
+    end
+  }
+end
+
+stats.each do |id, s|
+  puts "#{id} count=#{s[:count]} min=#{s[:min]} ms max=#{s[:max]} ms avg=#{s[:sum] / s[:count]} ms"
+end
